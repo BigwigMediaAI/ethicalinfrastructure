3c9bb70d8c81c1e45c5ffa59c7d6deb97dd3df39
unknown <chandanpms@gmail.com>
2025-11-01 18:43:51 +0530

    updated lead form

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-stretch gap-6 px-4 py-10">
         {/* Left: Google Map */}
         <div className="w-full lg:w-1/2">
-          <div className="w-full h-[250px] md:h-[480px] rounded-lg overflow-hidden shadow-lg">
+          <div className="w-full h-[250px] md:h-[470px] rounded-lg overflow-hidden shadow-lg">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.2037326384284!2d77.0934439!3d28.4734105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d18d7d137311f%3A0xc0cbc38c4452175d!2sETHICAL%20INFRASTRUCTURES%20PRIVATE%20LIMITED!5e0!3m2!1sen!2sin!4v1759831704667!5m2!1sen!2sin"
               className="w-full h-full border-0"
